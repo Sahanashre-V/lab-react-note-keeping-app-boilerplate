19461019bc5354fee9c6c497d9e2377db2daf6d9
V.Sahanashre <sahanashre.v@kalvium.community>
2024-01-29 17:44:48 +0530

    Added

---
+++ kalnote/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Data from "./Component/Editor";
+
+function App(){
+  return(
+    <div className="main">
+
+<Data/>
+
+    </div>
+  )
+}
+
+export default App
